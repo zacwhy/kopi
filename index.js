4fae5982ce29aca6aab9a39def2f077a74ec7004
ZY <zy@myrepublic.com.sg>
2016-10-13 10:22:47 +0800

    Prevent unauthorized modification of orders

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const
   app = require('express')(),
+  bodyParser = require('body-parser'),
   http = require('http').Server(app),
   io = require('socket.io')(http),
   config = require('./config.js');
@@ -10,29 +11,55 @@ let
   orders = {},
   products = config.products;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', socket => {
-  io.emit('products', products);
-  io.emit('orders', orders);
+app.post('/order', (req, res) => {
+  const
+    address = req.ip,
+    party = req.body.party,
+    product = req.body.product;
 
-  socket.on('product', product => {
-    products.push(product);
-    io.emit('products', products);
-  });
-
-  socket.on('order', (party, product) => {
+  if (typeof orders[party] !== 'undefined' && orders[party].address !== address) {
+    res.status(401)
+  } else {
     if (product !== '') {
-      orders[party] = product;
+      orders[party] = { address, product };
     } else {
       delete orders[party];
     }
-    io.emit('orders', orders);
+    emitOrders();    
+  }
+
+  res.end();
+});
+
+io.on('connection', socket => {
+  emitProducts();
+  emitOrders();
+
+  socket.on('product', product => {
+    products.push(product);
+    emitProducts();
   });
 });
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+function emitProducts() {
+  io.emit('products', products);
+}
+
+function emitOrders() {
+  const viewModel = Object.keys(orders).reduce((accumulator, key) => {
+    accumulator[key] = orders[key].product;
+    return accumulator;
+  }, {});
+  io.emit('orders', viewModel);
+}
